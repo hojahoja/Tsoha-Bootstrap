0f3df09fc69c64721460356030695fbded819d04
hojahoja <jari.honkonen@helsinki.fi>
2015-05-24 19:43:04 +0300

    sivuojen luonnokset

--- app/controllers/hello_world_controller.php
+++ app/controllers/hello_world_controller.php
@@ -4,11 +4,31 @@
 
     public static function index(){
       // make-metodi renderöi app/views-kansiossa sijaitsevia tiedostoja
-   	  View::make('home.html');
+   	  echo "etusivu";
     }
 
     public static function sandbox(){
       // Testaa koodiasi täällä
-      echo 'Hello World!';
+      View::make('helloworld.html');
+    }
+
+    public static function listing_page(){
+      View::make('plans/listingpage.html');
+    }
+
+    public static function note_information() {
+      View::make('plans/note.html');
+    }
+
+    public static function edit_note() {
+      View::make('plans/edit.html');
+    }
+
+    public static function login_page() {
+      View::make('plans/login.html');
+    }
+
+    public static function registration_page() {
+      View::make('plans/register.html');
     }
   }
--- config/routes.php
+++ config/routes.php
@@ -7,3 +7,23 @@
   $routes->get('/hiekkalaatikko', function() {
     HelloWorldController::sandbox();
   });
+
+  $routes->get('/task', function() {
+    HelloWorldController::listing_page();
+  });
+
+  $routes->get('/task/1', function() {
+    HelloWorldController::note_information();
+  });
+
+  $routes->get('/task/1/edit', function() {
+    HelloWorldController::edit_note();
+  });
+
+  $routes->get('/login', function() {
+    HelloWorldController::login_page();
+  });
+
+  $routes->get('/register', function() {
+    HelloWorldController::registration_page();
+  });
