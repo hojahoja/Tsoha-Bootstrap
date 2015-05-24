8eb843739a5c8fac73544d8114d07dea051cff46
hojahoja <jari.honkonen@helsinki.fi>
2015-05-24 23:49:25 +0300

    SQL

--- sql/add_test_data.sql
+++ sql/add_test_data.sql
@@ -1,1 +1,14 @@
--- Lisää INSERT INTO lauseet tähän tiedostoon
+INSERT INTO Kayttaja (Nimi, Salasana)
+VALUES ('Juri','Salasana');
+
+INSERT INTO Askare (Nimi, Kayttaja_id)
+SELECT 'Siivoa', id FROM Kayttaja
+WHERE Nimi = 'Juri';
+
+INSERT INTO Luokka (Nimi, Kayttaja_id)
+SELECT 'Pakollinen Paha', id FROM Kayttaja
+WHERE Nimi = 'Juri';
+
+INSERT INTO Tarkeysaste (Nimi, Aste, Kayttaja_id)
+SELECT 'Joskus ensivuonna', 10, id FROM Kayttaja
+WHERE Nimi = 'Juri';
--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -1,1 +1,35 @@
--- Lisää CREATE TABLE lauseet tähän tiedostoon
+CREATE TABLE Kayttaja(
+    id SERIAL PRIMARY KEY,
+    Nimi varchar(255) NOT NULL,
+    Salasana varchar(255) NOT NULL
+);
+
+CREATE TABLE Luokka(
+    id SERIAL PRIMARY KEY,
+    Nimi varchar(255) NOT NULL,
+    Kayttaja_id INTEGER REFERENCES Kayttaja(id)  ON DELETE cascade ON UPDATE cascade
+);
+
+CREATE TABLE Tarkeysaste(
+    id SERIAL PRIMARY KEY,
+    Nimi varchar(255) NOT NULL,
+    Aste integer,
+    Kayttaja_id INTEGER REFERENCES Kayttaja(id) ON DELETE cascade ON UPDATE cascade
+);
+
+CREATE TABLE Askare(
+    id SERIAL PRIMARY KEY,
+    Nimi varchar(255) NOT NULL,
+    Lisayspaiva DATE,
+    Tila boolean DEFAULT FALSE,
+    Kuvaus varchar(255),
+    Kayttaja_id INTEGER REFERENCES Kayttaja(id) ON DELETE cascade ON UPDATE cascade,
+    Tarkeysaste_id INTEGER REFERENCES Tarkeysaste(id) ON DELETE SET NULL ON UPDATE cascade
+);
+
+
+CREATE TABLE Askareenluokka(
+    Askare_id INTEGER REFERENCES Askare(id) ON DELETE cascade ON UPDATE cascade,
+    Luokka_id INTEGER REFERENCES Luokka(id) ON DELETE cascade ON UPDATE cascade,
+    PRIMARY KEY (Askare_id,Luokka_id)
+);
